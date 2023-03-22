b7b6afbbd6d080021eb40fd285a2ec650fc1de52
Alex <basurillodesgastorito@gmail.com>
2023-03-23 00:20:13 +0100

    a

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'WiiLink | A new online service for the Wii';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
